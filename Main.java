a78732f2d48032d696c77bc0f8916bb6be9b111a
Ishika <ish21112002@gmail.com>
2025-03-07 11:15:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+
+        // Java is case-sensitive
+        System.out.print("Hello World!");
+
+        // Print new line using \n
+        System.out.print("\nHow are you? ");
+
+        /* Another method to print new line
+        sout- shortcut to print System.out.println */
+        System.out.println("Nice to meet you");
+        System.out.println("I am working late because I am a singer");
+
+    }
+}
